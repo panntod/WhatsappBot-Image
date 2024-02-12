ea84270972a62087003f5313727d4884c1a27969
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-13 06:33:33 +0700

    refactor: added path to saved folder images and change format name

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { create, Client, decryptMedia, Message } from "@open-wa/wa-automate";
 import * as mime from "mime-types";
 import * as fs from "fs";
+import * as path from "path"
 
 function start(client: Client) {
   client.onAnyMessage(async (message: Message) => {
@@ -10,26 +11,38 @@ function start(client: Client) {
       message.quotedMsg
     ) {
       const quotedMessage = message.quotedMsg;
+      
       if (quotedMessage.mimetype) {
-        const filename = `${Date.now()}.${mime.extension(
+        const fileName = `Saved-${Date.now()}.${mime.extension(
           quotedMessage.mimetype
         )}`;
+        
         const mediaData = await decryptMedia(quotedMessage);
         const imageBase64 = `data:${
           quotedMessage.mimetype
         };base64,${mediaData.toString("base64")}`;
+
+        const imageFolderPath = path.join(__dirname, 'images')
+
+        if(!fs.existsSync(imageFolderPath){
+          fs.mkdirSync(imageFolderPath)
+        }
+
+        const finalImage = path.join(imageFolderPath, fileName)
+        
+        fs.writeFile(finalImage, mediaData, function (err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("File berhasil disimpan");
+        });
+
         await client.sendImage(
           message.chatId,
           imageBase64,
           filename,
           "Here's your fav photo"
         );
-        fs.writeFile(filename, mediaData, function (err) {
-          if (err) {
-            return console.log(err);
-          }
-          console.log("File berhasil disimpan");
-        });
       }
     }
   });
